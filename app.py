1ac72391892b554cc2d4c3e58c7d20025e77e8c1
peter942 <petoriaomalley@gmail.com>
2023-03-14 22:07:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1451,10 +1451,8 @@ def main():
         if "localhost" not in current_url:
             st.session_state["online"] = True            
         else:                                     
-             st.session_state["online"] = False     
-             st.session_state["welcome_state"] = app_settings["welcome_state"]
-             st.write(st.session_state["welcome_state"])
-                               
+             st.session_state["online"] = False                               
+        st.session_state["welcome_state"] = app_settings["welcome_state"]                       
     if st.session_state["online"] == True:
         st.error("**PLEASE READ:** This is a demo app. While you can click around, *buttons & queries won't work* and some things won't display properly. To use the proper version, follow the instructions [here](https://github.com/peter942/banodoco) to run it locally.")
     
