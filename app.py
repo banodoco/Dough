2d5442bc28e6e561005449c1482f43e10aa68588
peter942 <petoriaomalley@gmail.com>
2023-03-16 01:47:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1437,7 +1437,77 @@ def page_switcher(pages, page):
 
     return index_of_page_in_section, index_of_section
                 
+def resize_video(input_path, output_path, width, height, crop_type=None, output_format='mp4'):
+    # Read input video
+    input_video = cv2.VideoCapture(input_path)
+    if not input_video.isOpened():
+        raise ValueError(f"Could not open the video file: {input_path}")
 
+    # Get source video properties
+    src_width = int(input_video.get(cv2.CAP_PROP_FRAME_WIDTH))
+    src_height = int(input_video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = int(input_video.get(cv2.CAP_PROP_FPS))
+    num_frames = int(input_video.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    # Calculate aspect ratios
+    src_aspect_ratio = src_width / src_height
+    target_aspect_ratio = width / height
+
+    if target_aspect_ratio > src_aspect_ratio:
+        # Scale to target width, maintaining aspect ratio
+        new_width = width
+        new_height = int(src_height * (width / src_width))
+    else:
+        # Scale to target height, maintaining aspect ratio
+        new_width = int(src_width * (height / src_height))
+        new_height = height
+
+    # Determine the crop type based on the input dimensions, if not provided
+    if crop_type is None:
+        width_diff = abs(src_width - width) / src_width
+        height_diff = abs(src_height - height) / src_height
+        crop_type = 'top_bottom' if height_diff > width_diff else 'left_right'
+
+    # Calculate crop dimensions
+    if crop_type == 'top_bottom':
+        crop_top = (new_height - height) // 2
+        crop_bottom = new_height - crop_top
+        crop_left = 0
+        crop_right = new_width
+    elif crop_type == 'left_right':
+        crop_top = 0
+        crop_bottom = new_height
+        crop_left = (new_width - width) // 2
+        crop_right = new_width - crop_left
+    else:
+        raise ValueError("Invalid crop_type. Must be 'top_bottom' or 'left_right'.")
+
+    # Create output video
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    
+    # delete the video at the input_path
+    os.remove(input_path)
+
+    output_video = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
+
+    for _ in range(num_frames):
+        ret, frame = input_video.read()
+        if not ret:
+            break
+
+        # Resize frame
+        frame = cv2.resize(frame, (new_width, new_height))
+
+        # Crop frame
+        frame = frame[crop_top:crop_bottom, crop_left:crop_right]
+
+        # Write frame to output video
+        output_video.write(frame)
+
+    # Release resources
+    input_video.release()
+    output_video.release()
+    cv2.destroyAllWindows()
 
 def main():
 
@@ -1633,39 +1703,50 @@ def main():
                 project_settings = get_project_settings(project_name)                        
                 
 
-                st.sidebar.subheader("Extract key frames from video")                
-                input_video_list = [f for f in os.listdir(f'videos/{project_name}/assets/resources/input_videos') if f.endswith('.mp4')]            
-                if project_settings["input_video"] != "": 
-                    input_video_index = input_video_list.index(project_settings["input_video"])                
-                    input_video = st.sidebar.selectbox("Input video:", input_video_list, index = input_video_index)
-                    st.sidebar.video(f'videos/{project_name}/assets/resources/input_videos/{input_video}')
-                else:
-                    input_video = st.sidebar.selectbox("Input video:", input_video_list)
+                st.sidebar.subheader("Upload new videos")
+                st.sidebar.write("Open the toggle below to upload and select new inputs video to use for this project.")
+                          
 
-                if st.sidebar.button("Update Video"):
-                    update_project_setting("input_video", input_video, project_name)                
+                with st.sidebar.expander("Select input video", expanded=False):   
+                    input_video_list = [f for f in os.listdir(f'videos/{project_name}/assets/resources/input_videos') if f.endswith(('.mp4', '.mov','.MOV', '.avi'))]       
+                    if project_settings["input_video"] != "": 
+                        input_video_index = input_video_list.index(project_settings["input_video"])                
+                        input_video = st.selectbox("Input video:", input_video_list, index = input_video_index)
+                        st.video(f'videos/{project_name}/assets/resources/input_videos/{input_video}')
+                    else:
+                        input_video = st.selectbox("Input video:", input_video_list)
 
-                with st.sidebar.expander("Upload new video", expanded=False):                
+                    if st.button("Update Video"):
+                        update_project_setting("input_video", input_video, project_name)                   
+                        st.experimental_rerun()
+                    st.subheader("Upload new video")
                     width = int(project_settings["width"])
                     height = int(project_settings["height"])
-                    st.info("Make sure that this video is the same dimensions as your project - width: " + str(width) + "px, height: " + str(height) + "px.")
+                    
                     uploaded_file = st.file_uploader("Choose a file")
                     keep_audio = st.checkbox("Keep audio from original video.")
-                    if st.button("Upload video"):                
-                        with open(f'videos/{project_name}/assets/resources/input_videos/{uploaded_file.name}', 'wb') as f:
+                    resize_video = st.checkbox("Resize video to match project settings:" + str(width) + "px x " + str(height)+ "px", value=True)
+                    
+                    if st.button("Upload new video"):   
+                        video_path = f'videos/{project_name}/assets/resources/input_videos/{uploaded_file.name}'                
+                        with open(video_path, 'wb') as f:
                             f.write(uploaded_file.getbuffer())
+                         
                         width = int(project_settings["width"])
-                        height = int(project_settings["height"])                    
+                        height = int(project_settings["height"])
+                        resize_video(input_path=video_path,output_path=video_path,width=width,height=height)                    
                         st.success("Video uploaded successfully")
                         if keep_audio == True:
                             clip = VideoFileClip(f'videos/{project_name}/assets/resources/input_videos/{uploaded_file.name}')                    
                             clip.audio.write_audiofile(f'videos/{project_name}/assets/resources/audio/extracted_audio.mp3')
                             update_project_setting("audio", "extracted_audio.mp3", project_name)
-                        time.sleep(1.5)
+                        update_project_setting("input_video", input_video, project_name)                
+                        time.sleep(1)
                         st.experimental_rerun()
                         
                 st.sidebar.subheader("Bulk extract key frames from video")
-                st.sidebar.info("You can use either of the options below to extract key frames from your video or you can use the manual key from selector below. If need be, you can also refine your key frame selection after bulk extraction using the tool on the right.")
+                with st.sidebar.expander("💡 Learn about bulk extraction vs manual selection", expanded=False):
+                    st.info("You can use either of the options below to extract key frames from your video in bulk, or you can use the manual key on the bottom right. If need be, you can also refine your key frame selection after bulk extraction using by clicking into the single frame view.")
                 types_of_extraction = ["Regular intervals", "Extract from csv"]            
                     
                 type_of_extraction = st.sidebar.radio("Choose type of key frame extraction", types_of_extraction)
@@ -2018,17 +2099,19 @@ def main():
                     update_project_setting("width", width, new_project_name)
                     update_project_setting("height", height, new_project_name)  
                     update_project_setting("input_type", input_type, new_project_name)
+                    video_path = f'videos/{new_project_name}/assets/resources/input_videos/{uploaded_video.name}'
                     if uploaded_video is not None:
-                        with open(f'videos/{project_name}/assets/resources/input_videos/{uploaded_video.name}', 'wb') as f:
+                        with open(video_path, 'wb') as f:
                             f.write(uploaded_video.getbuffer())
                         update_project_setting("input_video", uploaded_video.name, new_project_name)
+                    resize_video(input_path=video_path,output_path=video_path,width=width,height=height)   
                     if audio == "Attach new audio":
                         if uploaded_audio is not None:
                             with open(os.path.join(f"videos/{new_project_name}/assets/resources/audio",uploaded_audio.name),"wb") as f: 
                                 f.write(uploaded_audio.getbuffer())
                             update_project_setting("audio", uploaded_audio.name, new_project_name)                
                     if audio == "Keep audio from original video":
-                        clip = VideoFileClip(f'videos/{new_project_name}/assets/resources/input_videos/{uploaded_video.name}')                    
+                        clip = VideoFileClip(video_path)                    
                         clip.audio.write_audiofile(f'videos/{new_project_name}/assets/resources/audio/extracted_audio.mp3')
                         update_project_setting("audio", "extracted_audio.mp3", new_project_name)                                            
                     st.session_state["project_name"] = new_project_name
