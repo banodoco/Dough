67c79fdf8fe8a34c252fc24d93583bf85109b33d
peter942 <peter@omalley.io>
2023-04-03 20:08:14 +0200

    Fixed issue with project switching

--- ui_components/setup.py
+++ ui_components/setup.py
@@ -21,16 +21,7 @@ from ui_components.components.video_rendering_page import video_rendering_page
 from ui_components.components.welcome_page import welcome_page
 
 def setup_app_ui():
-    def project_changed():
-        st.session_state["project_changed"] = True
-
-    if "project_changed" not in st.session_state:
-        st.session_state["project_changed"] = False
-
-    if st.session_state["project_changed"] == True:
-        update_app_settings("previous_project", st.session_state["project_name"])
-        st.session_state["project_changed"] = False        
-
+       
     app_settings = get_app_settings()
     title1, title2 = st.sidebar.columns([3,2])
     with title1:
@@ -41,34 +32,29 @@ def setup_app_ui():
            
     if int(st.session_state["welcome_state"]) in [0,1,2,3,4] and st.session_state["online"] == False:
         welcome_page()
-    else:
-        if "project_set" not in st.session_state:
-            st.session_state["project_set"] = "No"
-            st.session_state["page_updated"] = "Yes"
-
-        if st.session_state["project_set"] == "Yes":
-            st.session_state["index_of_project_name"] = os.listdir("videos").index(st.session_state["project_name"])
-            st.session_state["project_set"] = "No"
+    else:                
+        video_list = [f for f in os.listdir("videos") if not f.startswith('.')]
+        if "index_of_project_name" not in st.session_state:
+            if app_settings["previous_project"] != "":
+                st.session_state["project_name"] = app_settings["previous_project"] 
+                st.session_state["index_of_project_name"] = video_list.index(app_settings["previous_project"])            
+            else:
+                st.session_state["index_of_project_name"] = 0
+                
+        st.session_state["project_name"] = st.sidebar.selectbox("Select which project you'd like to work on:", video_list,index=st.session_state["index_of_project_name"])  
+        
+        if st.session_state["index_of_project_name"] != video_list.index(st.session_state["project_name"]):
+            st.write("Project changed")
+            st.session_state["index_of_project_name"] = video_list.index(st.session_state["project_name"])               
+            update_app_settings("previous_project", st.session_state["project_name"])
             st.experimental_rerun()
-        
-        if app_settings["previous_project"] != "":
-            st.session_state["project_name"] = app_settings["previous_project"]
-            video_list = os.listdir("videos")
-            st.session_state["index_of_project_name"] = video_list.index(st.session_state["project_name"])
-            st.session_state['project_set'] = 'No'
-        else:
-            # st.session_state["project_name"] = project_name
-            st.session_state["index_of_project_name"] = 0
-        
-        st.session_state["project_name"] = st.sidebar.selectbox("Select which project you'd like to work on:", os.listdir("videos"),index=st.session_state["index_of_project_name"], on_change=project_changed())    
-        project_name = st.session_state["project_name"]
-        
-        if project_name == "":
+                
+        if st.session_state["project_name"] == "":
             st.info("No projects found - create one in the 'New Project' section")
         else:  
 
-            if not os.path.exists("videos/" + project_name + "/assets"):
-                create_working_assets(project_name)
+            if not os.path.exists("videos/" + st.session_state["project_name"] + "/assets"):
+                create_working_assets(st.session_state["project_name"])
             
             if "index_of_section" not in st.session_state:
                 st.session_state["index_of_section"] = 0
@@ -95,7 +81,7 @@ def setup_app_ui():
 
             
 
-            timing_details = get_timing_details(project_name)
+            timing_details = get_timing_details(st.session_state["project_name"])
 
             st.session_state["section"] = st.sidebar.radio("Select a section:", [page["section_name"] for page in pages],horizontal=True)  
             st.session_state["page"] = st.sidebar.radio("Select a page:", [page for page in pages if page["section_name"] == st.session_state["section"]][0]["pages"],horizontal=False)
@@ -107,28 +93,28 @@ def setup_app_ui():
 
             # APP ROUTING
             if st.session_state["page"] == "Key Frame Selection":
-                key_frame_selection_page(mainheader2, project_name)
+                key_frame_selection_page(mainheader2, st.session_state["project_name"])
             elif st.session_state["page"] == "App Settings":
                 app_settings_page()
             elif st.session_state["page"] == "New Project":
                 new_project_page()
             elif st.session_state["page"] == "Frame Styling":
-                frame_styling_page(mainheader2, project_name)
+                frame_styling_page(mainheader2, st.session_state["project_name"])
             elif st.session_state["page"] == "Frame Interpolation":
-                frame_interpolation_page(mainheader2, project_name)
+                frame_interpolation_page(mainheader2, st.session_state["project_name"])
             elif st.session_state["page"] == "Video Rendering":
-                video_rendering_page(mainheader2, project_name)
+                video_rendering_page(mainheader2, st.session_state["project_name"])
             elif st.session_state["page"] == "Batch Actions":
-                batch_action_page(project_name)
+                batch_action_page(st.session_state["project_name"])
             elif st.session_state["page"] == "Project Settings":
-                project_settings_page(project_name)
+                project_settings_page(st.session_state["project_name"])
             elif st.session_state["page"] == "Custom Models":
-                custom_models_page(project_name)
+                custom_models_page(st.session_state["project_name"])
             elif st.session_state["page"] == "Frame Editing":
-                frame_editing_page(project_name)
+                frame_editing_page(st.session_state["project_name"])
             elif st.session_state["page"] == "Timing Adjustment":
-                timing_adjustment_page(project_name)                                     
+                timing_adjustment_page(st.session_state["project_name"])                                     
             elif st.session_state["page"] == "Prompt Finder":
-                prompt_finder_page(project_name)
+                prompt_finder_page(st.session_state["project_name"])
             else:
                 st.info("You haven't added any prompts yet. Add an image to get started.")
