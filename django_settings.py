b1fe13e6bae1af6fd97abb41db925c432e465e2a
piyushK52 <piyush.kumar.iaf@gmail.com>
2023-11-07 18:12:28 +0530

    hosting flag added

--- django_settings.py
+++ django_settings.py
@@ -7,7 +7,7 @@ sys.path.append('../')
 
 from dotenv import load_dotenv
 
-from shared.constants import LOCAL_DATABASE_NAME, SERVER, ServerType
+from shared.constants import HOSTED_BACKGROUND_RUNNER_MODE, LOCAL_DATABASE_NAME, SERVER, ServerType
 
 
 load_dotenv()
@@ -19,12 +19,32 @@ else:
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': DB_LOCATION,
+if not HOSTED_BACKGROUND_RUNNER_MODE:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': DB_LOCATION,
+        }
     }
-}
+else:
+    import boto3
+    ssm = boto3.client("ssm", region_name="ap-south-1")
+    DB_NAME = ssm.get_parameter(Name='/backend/banodoco/db/name')['Parameter']['Value']
+    DB_USER = ssm.get_parameter(Name='/backend/banodoco/db/user')['Parameter']['Value']
+    DB_PASS = ssm.get_parameter(Name='/backend/banodoco/db/password')['Parameter']['Value']
+    DB_HOST = ssm.get_parameter(Name='/backend/banodoco/db/host')['Parameter']['Value']
+    DB_PORT = ssm.get_parameter(Name='/backend/banodoco/db/port')['Parameter']['Value']
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': DB_NAME,
+            'USER': DB_USER,
+            'PASSWORD': DB_PASS,
+            'HOST': DB_HOST,
+            'PORT': DB_PORT,
+        }
+    }
 
 INSTALLED_APPS = ('backend',)
 
