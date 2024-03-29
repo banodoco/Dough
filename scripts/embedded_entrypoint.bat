ca130cd8bfd7adbe402da066a77b8264fc060ffb
piyushk52 <piyush.kumar.iaf@gmail.com>
2024-03-29 11:43:31 -0700

    wip: portable version

--- scripts/embedded_entrypoint.bat
+++ scripts/embedded_entrypoint.bat
@@ -1,1 +1,1 @@
-.\python-3.10.10-embed-amd64\Scripts\streamlit.exe run app.py --runner.fastReruns false --server.port 5500
+..\python-3.10.10-embed-amd64\python.exe ..\python-3.10.10-embed-amd64\Scripts\streamlit.exe run app.py --runner.fastReruns false --server.port 5500
